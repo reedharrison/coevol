49341be89d2cf082fe1bc6e6665962c3c8eb8cea
reedharrison <rharr009@ucr.edu>
2016-06-17 17:47:55 +0200

    bug fixes, implemented Wilcoxon rank-sum test for selecting principle axes

--- coevol/coevol.py
+++ coevol/coevol.py
@@ -12,7 +12,7 @@ try:
 	import numpy as np
 	import pandas as pd
 	from Bio import SeqIO
-	from scipy import linalg
+	from scipy import linalg, stats
 except:
 	print('ERROR: required libraries could not be loaded')
 
@@ -136,6 +136,8 @@ class mca:
 	key_col
 		List of keys for each column in notation "l.val" where l is the column index from W and val is 
 		the unique value that is masked from W[:,l]. Note that l is not unique across keys.
+	key_lbl
+		List of keys for sequence labels in same format as key_col
 	Z
 		data of X transformed
 	V
@@ -168,7 +170,7 @@ class mca:
 
 	"""
 
-	def __init__(self, W, phi=None, row=False, col=False):
+	def __init__(self, W, phi=None, row=False, col=False, tol=0.00001):
 		"""Summary
 
 		Parameters
@@ -183,11 +185,14 @@ class mca:
 			project rows onto principle axes if True
 		proj_col :
 			project columns onto principle axes if True
+		tol:
+			eigenvalues falling within the range of 1-tol<y<1+tol will be discarded as trivial
 
 		"""
-		self.phi = phi
+		self.phi, self.key_lbl = binarize(phi, axis=1, lbl=True)
 		self.row = row
 		self.col = col
+		self.tol = tol
 
 		self.L 	= W.shape[1]
 		self.N 	= W.shape[0]
@@ -198,9 +203,9 @@ class mca:
 		self.P 			= X.shape[1]
 		P 				= self.P
 
-		x_nS = X.sum(axis=0)
-		x_Sp = X.sum(axis=1)
-		x_SS = X.sum()
+		x_nS = X.sum(axis=1) 	# Row sums
+		x_Sp = X.sum(axis=0) 	# Col sums
+		x_SS = X.sum()			# Array sum
 
 		f_nS = x_nS/x_SS
 		f_Sp = x_Sp/x_SS
@@ -210,8 +215,8 @@ class mca:
 		self.f_Sp = f_Sp
 		self.f_np = f_np
 
-		xv, yv 	= np.meshgrid(f_nS, f_Sp)
-		Z 		= f_np / (xv * yv)
+		xv, yv 	= np.meshgrid(f_Sp, f_nS)
+		Z 		= f_np / np.sqrt(xv * yv)
 		self.Z 	= Z
 
 	def decompose(self):
@@ -221,14 +226,22 @@ class mca:
 		"""
 		Z = self.Z
 		L = self.L
+		tol = self.tol
 
 		w, v = linalg.eigh(a=np.dot(Z,Z.T))
 
-		chk1 	= w != 1
-		chk2 	= w != 0
-		chk3 	= w > (1/(L**2))
+# Notes: filtering out trivial eigenvalues was not working without tolerance implementation
+		# chk1 	= w != 1
+		# chk2 	= w != 0
+		# chk3 	= w > (1/(L**2))
+		# mask 	= np.logical_and(chk1, chk2)
+		# mask 	= np.logical_and(mask, chk3)
+		# w 		= w[mask]
+		# v 		= v[:,mask]
+
+		chk1 	= np.logical_or(w < 1-tol, w > 1+tol)
+		chk2 	= w >= (1/(L**2))
 		mask 	= np.logical_and(chk1, chk2)
-		mask 	= np.logical_and(mask, chk3)
 		w 		= w[mask]
 		v 		= v[:,mask]
 
@@ -247,9 +260,13 @@ class mca:
 		# self.D = D
 		# self.D_adj = D_adj
 
-	def percent_variance(self):
+	def percent_variance(self, mode=None):
 		y = self.y_adj
-		return 100*y/y.sum()
+		variance = 100*y/y.sum()
+		if mode is None:
+			return np.fliplr(variance)
+		else:
+			return np.fliplr(variance)[mode]
 
 	def proj_row(self):
 		"""Summary
@@ -276,8 +293,17 @@ class mca:
 		np.fill_diagonal(D, np.sqrt(y))
 		np.fill_diagonal(D_adj, np.sqrt(y_adj))
 
-		theta 		= np.sqrt(N)*(np.dot(V, D_adj))
-		self.theta 	= theta
+		R 		= np.mat(R)
+		T 		= np.mat(T)
+		D 		= np.mat(D)
+		D_adj 	= np.mat(D_adj)
+		V 		= np.mat(V)
+
+		# theta 		= np.sqrt(N)*(np.dot(V, D_adj))
+		theta 		= np.sqrt(N)*(V * D_adj)
+		theta_std	= np.sqrt(N)*(V * D.I * D_adj)
+		self.theta 	= theta.A
+		self.theta_std = theta_std.A
 
 	def proj_col(self):
 		"""Summary
@@ -300,13 +326,21 @@ class mca:
 		D 		= np.zeros((J,J))
 		D_adj 	= np.zeros((J,J))
 
+		R 		= np.mat(R)
+		T 		= np.mat(T)
+		D 		= np.mat(D)
+		D_adj 	= np.mat(D_adj)
+		Z 		= np.mat(Z)
+		V 		= np.mat(V)
+
 		np.fill_diagonal(R, np.sqrt(f_nS))
 		np.fill_diagonal(T, np.sqrt(f_Sp))
 		np.fill_diagonal(D, np.sqrt(y))
 		np.fill_diagonal(D_adj, np.sqrt(y_adj))
 
-		psi 		= np.dot(np.dot(np.dot(T , np.dot(Z.T, V)), D.T), D_adj)
-		self.psi 	= psi
+		# psi 		= np.dot(np.dot(np.dot(T , np.dot(Z.T, V)), D.I), D_adj)
+		psi 		= T * (Z.I * V) * D.I * D_adj
+		self.psi 	= psi.A
 
 	def proj_feat(self):
 		"""Summary
@@ -319,10 +353,11 @@ class mca:
 		Z 	= self.Z
 		V 	= self.V
 		phi = self.phi
-		if len(phi.shape) == 1:
-			C = 1
-		if len(phi.shape) > 1:
-			C = phi.shape[1]
+		# if len(phi.shape) == 1: #17/6/2016, removed as should be unneccesary if binarizing labels
+		# 	C = 1
+		# if len(phi.shape) > 1:
+		# 	C = phi.shape[1]
+		C = phi.shape[1]
 
 		f_nS 	= self.f_nS
 		f_Sp 	= self.f_Sp
@@ -334,27 +369,113 @@ class mca:
 		D 		= np.zeros((J,J))
 		D_adj 	= np.zeros((J,J))
 
+		R 		= np.mat(R)
+		H 		= np.mat(H)
+		D 		= np.mat(D)
+		D_adj 	= np.mat(D_adj)
+		phi		= np.mat(phi)
+		V 		= np.mat(V)
+
 		np.fill_diagonal(R, np.sqrt(f_nS))
-		np.fill_diagonal(C, phi.sum(axis=0))
+		np.fill_diagonal(H, phi.sum(axis=0))
 		np.fill_diagonal(D, np.sqrt(y))
 		np.fill_diagonal(D_adj, np.sqrt(y_adj))
 
-		omega = np.dot(np.dot(np.dot(np.dot(np.dot(R, phi), H).T, V), D.T), D_adj)
-		self.omega = omega
+		# omega = np.dot(np.dot(np.dot(np.dot(np.dot(R, phi), H).T, V), D.I), D_adj)
+		omega = ((R * phi * H).T * V) * D.I * D_adj
+		self.omega = omega.A
 
 	def calc(self):
 		"""Summary
 		Carries out mca analysis. User only need interact with this class method after instantiation of a mca class.
 
 		"""
-
 		self.decompose()
 		if self.row:
 			self.proj_row()
 		if self.col:
 			self.proj_col()
-		if self.c is not None:
+		if self.phi is not None:
 			self.proj_feat()
+
+	def evec(self, mode=None):
+		if mode is None:
+			return np.fliplr(self.V)
+		else:
+			return np.fliplr(self.V)[:,mode]
+	def eval(self, mode=None):
+		if mode is None:
+			return np.fliplr(self.y_adj)
+		else:
+			return np.flipud(self.y_adj)[mode]
+	def eval_raw(self, mode=None):
+		if mode is None:
+			return np.fliplr(self.y)
+		else:
+			return np.flipud(self.y)[mode]
+	def score_row(self, mode=None):
+		if mode is None:
+			return np.fliplr(self.theta)
+		else:
+			return np.fliplr(self.theta)[:,mode]
+	def score_col(self, mode=None):
+		if mode is None:
+			return np.fliplr(self.psi)
+		else:
+			return np.fliplr(self.psi)[:,mode]
+	def score_feat(self, mode=None):
+		if mode is None:
+			return np.fliplr(self.omega)
+		else:
+			return np.fliplr(self.omega)[:,mode]
+
+	def findI(self, alpha=0.01, use_continuity=True):#, zero_method='wilcox', correction=False):
+		"""Summary
+		Use the Wilcoxon rank-sum test to find the first I principle axes that contribute significant information.
+		The method will properly re-order principle axes assuming the variables were not modified by the end-user.
+
+		Parameters
+		----------
+		alpha
+			critical p-value for determining if collection of columns is significant
+		use_continuity
+			parameter for scipy.stats.mannwhitneyu. if true, the method performs a continuity correction of 1/2.
+
+		"""
+		J = self.J
+		N = self.N
+		theta = np.fliplr(self.theta)
+		y = np.flipud(self.y_adj)
+
+		# Test to set max I is not working properly
+		# lhs = np.empty((J,))
+		# rhs = np.empty((J,))
+		# for j in xrange(J):
+		#     if j != 0:
+		#         lhs[j] = y[0:j].sum()
+		#         rhs[j] = (theta**2).sum(axis=0)[0:j].sum()
+		#     if j == 0:
+		#         lhs[j] = y[j]
+		#         rhs[j] = (theta ** 2).sum(axis=0)[j]
+		# diff = np.abs(lhs-rhs)
+		# Imax = np.max([np.argmin(diff)+1, 2])
+		# Imax = np.min([Imax, len(lhs)])
+
+		pval = np.ones((len(range(1,J)),))
+		for i in xrange(1,J):
+			x = (theta**2)[:,0:i-1].sum(axis=1)
+			y = (theta**2)[:,0:i].sum(axis=1)
+			# results = stats.ranksums(x,y) # Other similar methods that are not preferred
+			# results = stats.wilcoxon(x, y, zero_method=zero_method, correction=correction)
+			# pval[i-1] = stats.norm.cdf(results[0])
+			results = stats.mannwhitneyu(x, y, use_continuity=use_continuity)
+			pval[i-1] = results[1]
+
+		mask = (pval < alpha)
+		I = np.where(mask==False)[0][0]
+		self.I = I
+		self.pval = pval
+		return I
 
 
 """
@@ -394,7 +515,7 @@ def binarize(data, axis=1, lbl=False):
 	for i in xrange(n_feat):
 		if (n_feat == 1):
 			v = data
-			axis = np.argmin(v.shape)
+			# axis = np.argmax(v.shape)
 		if (axis == 0) and (n_feat != 1):
 			v = data[i,:]
 		if (axis == 1) and (n_feat != 1):
